4b688d8af35fad49d64e71b190716b54b7cb1a07
Josh Jennings <josh.jennings.85@gmail.com>
2015-11-25 08:29:46 -0600

    Imported project into IntelliJ JetBrains. Converted ActionListener for the Connect button to a Lambda expression. "for" loop changed to "foreach". setDefaultCloseOperation now uses Swing field WindowConstants. try-catch now catches NumberFormatException instead of generic Exception.

--- Sensor Graph/src/SensorGraph.java
+++ Sensor Graph/src/SensorGraph.java
@@ -3,10 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -27,7 +24,7 @@ public class SensorGraph {
 		window.setTitle("Sensor Graph GUI");
 		window.setSize(600,400);
 		window.setLayout(new BorderLayout());
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// create drop down for connection options
 		JComboBox<String> portList = new JComboBox<String>();
@@ -39,9 +36,9 @@ public class SensorGraph {
 		
 		//populate drop down
 		SerialPort[] portNames = SerialPort.getCommPorts(); //collect port names into array
-		for (int i = 0; i < portNames.length; i++) {
+		for (SerialPort portName : portNames) {
 			//add each port name item to the list of ports
-			portList.addItem(portNames[i].getSystemPortName());
+			portList.addItem(portName.getSystemPortName());
 		}
 		
 		// create line graph
@@ -52,48 +49,46 @@ public class SensorGraph {
 		window.add(new ChartPanel(chart), BorderLayout.CENTER);
 		
 		// configure Connect button - use another thread to listen for data
-		connectButton.addActionListener(new ActionListener(){
-			@Override public void actionPerformed(ActionEvent e) {
-				if(connectButton.getText().equals("Connect")) {
-					//attempt connection
-					chosenPort = SerialPort.getCommPort(portList.getSelectedItem().toString());
-					chosenPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
-					//attempt to open the port - if successful, set new text and disable drop down
-					if (chosenPort.openPort()) {
-						connectButton.setText("Disconnect");
-						portList.setEnabled(false);
-					}
-					
-					//create a new thread that listens on serial
-					Thread thread = new Thread(){
-						@Override public void run() {
-							//define new Scanner receiving serial input
-							Scanner scanner = new Scanner(chosenPort.getInputStream());
-							while (scanner.hasNextLine()) {
-								//if stream has another line, parse and add to series
-								try {
-									String line = scanner.nextLine(); //get text
-									int number = Integer.parseInt(line); //parse text into integer
-									series.add(x++, 1023 - number);	//add int to data series
-								} catch(Exception e) {} //if error, skip
-							} //if no more lines, exit loop and close Scanner
-							scanner.close();
-						}
-					};
-					thread.start();
-				} else { //else, if button text equals "Disconnect" then perform these tasks
-					//disconnect from serial port
-					chosenPort.closePort();
-					//re-enable list of ports
-					portList.setEnabled(true);
-					//switch button text back to "Connect"
-					connectButton.setText("Connect");
-					//if wanted to clear data: series.clear(); /n x = 0;
-					// TODO: add button to clear data that is active only when stopped
-				}
-			}
-			
-		});
+		connectButton.addActionListener((ActionEvent e) -> {
+            if(connectButton.getText().equals("Connect")) {
+                //attempt connection
+                chosenPort = SerialPort.getCommPort(portList.getSelectedItem().toString());
+                chosenPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
+                //attempt to open the port - if successful, set new text and disable drop down
+                if (chosenPort.openPort()) {
+                    connectButton.setText("Disconnect");
+                    portList.setEnabled(false);
+                }
+
+                //create a new thread that listens on serial
+                Thread thread = new Thread(){
+                    @Override public void run() {
+                        //define new Scanner receiving serial input
+                        Scanner scanner = new Scanner(chosenPort.getInputStream());
+                        while (scanner.hasNextLine()) {
+                            //if stream has another line, parse and add to series
+							try {
+								String line = scanner.nextLine(); //get text
+								int number = Integer.parseInt(line); //parse text into integer
+								series.add(x++, 1023 - number);    //add int to data series
+							} catch (NumberFormatException ignored) {
+							}
+                        } //if no more lines, exit loop and close Scanner
+                        scanner.close();
+                    }
+                };
+                thread.start();
+            } else { //else, if button text equals "Disconnect" then perform these tasks
+                //disconnect from serial port
+                chosenPort.closePort();
+                //re-enable list of ports
+                portList.setEnabled(true);
+                //switch button text back to "Connect"
+                connectButton.setText("Connect");
+                //if wanted to clear data: series.clear(); /n x = 0;
+                // TODO: add button to clear data that is active only when stopped
+            }
+        });
 		
 		// display window
 		window.setVisible(true);
